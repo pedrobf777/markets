094cb34405a4ea582e2f0a192456fa560313c768
Brenton Cleeland <brenton@brntn.me>
2016-06-25 12:14:28 +1000

    Chance of winning via Betfair

---
+++ betfair.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_betfair_chances(url):
+    chances = []
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    runners = soup.findAll(attrs={'class': 'runner'})
+
+    for runner in runners:
+        name = runner.find(attrs={'class': 'selection-name'}).text.strip()
+        prices = runner.findAll(attrs={'class': 'default-price'})
+
+        if len(prices) != 2:
+            continue
+
+        back = float(prices[0].text.strip().splitlines()[0])
+
+        try:
+            lay = float(prices[1].text.strip().splitlines()[0])
+        except IndexError:
+            lay = back
+
+        chance = ((1 / back + 1 / lay) / 2) * 100
+
+        if chance > 0.5:
+            chances.append((name, chance))
+
+    return chances
---
+++ elections.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import csv
+from datetime import datetime
+from jinja2 import Template
+
+from betfair import get_betfair_chances
+
+
+markets = {
+    'au': {
+        'url': 'https://www.betfair.com.au/sports/politics/australian-politics/2215338/federal-election-winner/nonsport/2.100757409',
+        'name': 'Australian Federal Election',
+    },
+    'usa': {
+        'url': 'https://www.betfair.com.au/sports/politics/2016-us-presidential-election/8887484/next-president/nonsport/1.107373419',
+        'name': 'Next US President'
+    }
+}
+
+
+def election():
+    template = Template(open('template.html').read())
+    for key, market in markets.items():
+        runners = get_betfair_chances(market['url'])
+        rendered = template.render(market=market['name'], runners=runners, markets=markets, market_url=market['url'])
+
+        with open('out/{}.html'.format(key), 'w') as f:
+            f.write(rendered)
+
+        with open('out/history.csv', 'a') as f:
+            now = datetime.now()
+            writer = csv.writer(f)
+            for runner in runners:
+                writer.writerow([now, key, runner[0], runner[1]])
+
+if __name__ == '__main__':
+    election()
