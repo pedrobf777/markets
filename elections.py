4f0d46257b14660b567a622927008139870b566f
Brenton Cleeland <brenton@brntn.me>
2016-06-27 16:46:29 +1000

    flake8

--- elections.py
+++ elections.py
@@ -9,11 +9,11 @@ from betfair import get_betfair_chances
 
 markets = {
     'au': {
-        'url': 'https://www.betfair.com.au/sports/politics/australian-politics/2215338/federal-election-winner/nonsport/2.100757409',
+        'url': 'https://www.betfair.com.au/sports/politics/australian-politics/2215338/federal-election-winner/nonsport/2.100757409',  # noqa
         'name': 'Australian Federal Election',
     },
     'usa': {
-        'url': 'https://www.betfair.com.au/sports/politics/2016-us-presidential-election/8887484/next-president/nonsport/1.107373419',
+        'url': 'https://www.betfair.com.au/sports/politics/2016-us-presidential-election/8887484/next-president/nonsport/1.107373419',  # noqa
         'name': 'Next US President'
     }
 }
@@ -23,7 +23,11 @@ def election():
     template = Template(open('template.html').read())
     for key, market in markets.items():
         runners = get_betfair_chances(market['url'])
-        rendered = template.render(market=market['name'], runners=runners, markets=markets, market_url=market['url'])
+        rendered = template.render(
+            market=market['name'],
+            runners=runners,
+            markets=markets,
+            market_url=market['url'])
 
         with open('out/{}.html'.format(key), 'w') as f:
             f.write(rendered)
