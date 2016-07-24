0c5d78b3ebe3346a45d1a4335c4348553754fc8b
Brenton Cleeland <brenton@brntn.me>
2016-07-24 19:02:26 +1000

    :bicyclist: Remove Le Tour

--- elections.py
+++ elections.py
@@ -12,10 +12,6 @@ markets = {
         'url': 'https://www.betfair.com.au/sports/politics/2016-us-presidential-election/8887484/next-president/nonsport/1.107373419',  # noqa
         'name': 'Next US President'
     },
-    'tour': {
-        'url': 'https://www.betfair.com.au/sports/cycling/tour-de-france-2016/7979859/winner/outright_winner/1.119710618',
-        'name': 'Tour de France'
-    }
 }
 
 
@@ -28,7 +24,7 @@ def election():
             runners=runners,
             markets=markets,
             market_url=market['url'],
-            updated=datetime.now().strftime('%Y-%m-%d %H:%M'))
+            updated=datetime.now().strftime('%Y-%m-%d %H:%M%z'))
 
         with open('out/{}.html'.format(key), 'w') as f:
             f.write(rendered)
